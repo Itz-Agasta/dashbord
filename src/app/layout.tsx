392b2d5cadd2f60a0764a20d556afa9685036be9
Itz-Agasta <rupamgolui69@gmail.com>
2025-07-04 01:54:56 +0530

    feat: refactor RootLayout to include SidebarProvider and manage sidebar state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import "./globals.css";
+import { cookies } from "next/headers";
 import AppSidebar from "@/components/AppSidebar";
 import Navbar from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
-export default function RootLayout({
+export default async function RootLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const cookieStore = await cookies();
+	const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body>
@@ -17,11 +21,13 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
-					<AppSidebar />
-					<main className="w-full">
-						<Navbar />
-						<div className="px-4">{children}</div>
-					</main>
+					<SidebarProvider defaultOpen={defaultOpen}>
+						<AppSidebar />
+						<main className="w-full">
+							<Navbar />
+							<div className="px-4">{children}</div>
+						</main>
+					</SidebarProvider>
 				</ThemeProvider>
 			</body>
 		</html>
