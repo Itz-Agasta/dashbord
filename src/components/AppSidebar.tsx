574faeb3c42a5bd21ff390b6791ecfef7d1facbc
Itz-Agasta <rupamgolui69@gmail.com>
2025-07-04 01:54:50 +0530

    feat: implement AppSidebar with menu items, project actions, and user dropdown

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,5 +1,168 @@
+import {
+	Calendar,
+	ChevronUp,
+	Home,
+	Inbox,
+	Plus,
+	Projector,
+	Search,
+	Settings,
+	User2,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupAction,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSubButton,
+	SidebarSeparator,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+	{
+		title: "Home",
+		url: "/",
+		icon: Home,
+	},
+	{
+		title: "Inbox",
+		url: "#",
+		icon: Inbox,
+	},
+	{
+		title: "Calendar",
+		url: "#",
+		icon: Calendar,
+	},
+	{
+		title: "Search",
+		url: "#",
+		icon: Search,
+	},
+	{
+		title: "Settings",
+		url: "#",
+		icon: Settings,
+	},
+];
+
 const AppSidebar = () => {
-	return <div className="">AppSidebar</div>;
+	return (
+		<Sidebar collapsible="icon">
+			{/* Header */}
+			<SidebarHeader className="p-4">
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton asChild>
+							<Link href="/">
+								<Image
+									src="https://avatars.githubusercontent.com/u/142140947"
+									alt="logo"
+									width={20}
+									height={20}
+									className="rounded-full"
+								/>
+								<span>Agasta</span>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarSeparator />
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel>Application</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<a href={item.url}>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+				<SidebarSeparator />
+				{/* Action sec */}
+				<SidebarGroup>
+					<SidebarGroupLabel>Projects</SidebarGroupLabel>
+					<SidebarGroupAction>
+						<Plus /> <span className="sr-only">Add Project</span>
+					</SidebarGroupAction>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							<SidebarMenuItem>
+								<SidebarMenuSubButton asChild>
+									<Link href="/#">
+										<Projector />
+										See All projects
+									</Link>
+								</SidebarMenuSubButton>
+							</SidebarMenuItem>
+							<SidebarMenuItem>
+								<SidebarMenuSubButton asChild>
+									<Link href="/#">
+										<Plus />
+										Add project
+									</Link>
+								</SidebarMenuSubButton>
+							</SidebarMenuItem>
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+			{/* Footer Sec*/}
+			<SidebarFooter>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<SidebarMenuButton>
+									<User2 /> Agasta
+									<ChevronUp className="ml-auto" />
+								</SidebarMenuButton>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent
+								side="top"
+								className="w-[--radix-popper-anchor-width]"
+							>
+								<DropdownMenuItem>
+									<span>Account</span>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<span>Settings</span>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<span>Sign out</span>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
+		</Sidebar>
+	);
 };
 
 export default AppSidebar;
